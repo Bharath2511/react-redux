5a65f3ab94aacae84e4395f386bbec091feeeaf5
V S Bharath <bharathcse5t6@gmail.com>
2018-10-18 18:33:27 +0530

    playing with forms

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Minions from './minions'
+import AddMinion from './AddMinion'
 
 
 class App extends Component {
   state = {
-    minions: [  { name:"Bob", age:"25", id:"0"},
-    { name:"kevin", age:"20", id:"1"},
-    { name:"Mario", age:"15", id:"2"}
+    minions: [  { name:"Bob", age:"25",belt:"black", id:"0"},
+    { name:"kevin", age:"20",belt:"blue", id:"1"},
+    { name:"Mario", age:"15",belt:"green", id:"2"}
 ] }
   render() {
     return (
@@ -14,6 +15,7 @@ class App extends Component {
         <h1>React</h1>
         <p>Welcome</p>
         <Minions minions={this.state.minions} />
+        <AddMinion />
      </div>
     );
   }
