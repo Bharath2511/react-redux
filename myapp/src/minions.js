83f11edba63aee87b13c5618d49048ef0f3fe029
V S Bharath <bharathcse5t6@gmail.com>
2018-10-18 16:47:46 +0530

    nested components

---
+++ myapp/src/minions.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+
+class Minions extends Component {
+    render() {
+        return (
+            <div className="ninja">
+             <div>name : Bob</div>
+             <div>Age : 30</div>
+             <div>Belt: "Black</div>
+            </div>
+        )
+    }
+}
+
+export default Minions
